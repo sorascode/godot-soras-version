58070af19ab1273b3d18898c36ea3c356cadb93b
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2025-04-01 19:00:42 +0200

    Add `profiler` option to `SCons` builds.     Add `tracy` option to `profiler`. If set, a tracy profiling client will be injected into the Godot binary.
    # Conflicts:
    #	platform/linuxbsd/godot_linuxbsd.cpp
    
    (cherry picked from commit e80194e31f6944d09afd04fd7298fb09a3bc2b52)
    
    # Conflicts:
    #	platform/android/java_godot_lib_jni.cpp
    #	platform/macos/os_macos.mm

--- platform/macos/os_macos.mm
+++ platform/macos/os_macos.mm
@@ -39,6 +39,7 @@
 #import "godot_application_delegate.h"
 
 #include "core/crypto/crypto_core.h"
+#include "core/profiling/profiling.h"
 #include "core/version_generated.gen.h"
 #include "drivers/apple/os_log_logger.h"
 #include "main/main.h"
@@ -1065,6 +1066,8 @@ static void handle_interrupt(int sig) {
 }
 
 void OS_MacOS_NSApp::start_main() {
+	godot_init_profiler();
+
 	Error err;
 	@autoreleasepool {
 		err = Main::setup(execpath, argc, argv);
