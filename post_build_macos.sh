b92fa970dcb4625a3d173ef0a5841d48f62e7988
sora <sora.herdlicka@gmail.com>
2025-07-30 10:33:49 +0200

    feat: add collision callback validation and add contact point to callback

--- post_build_macos.sh
+++
@@ -1,4 +0,0 @@
-rm -rf Godot.app/Contents/MacOS/Godot
-cp bin/godot.macos.editor.arm64 Godot.app/Contents/MacOS/Godot
-chmod +x Godot.app/Contents/MacOS/Godot
-codesign --force --timestamp --options=runtime --entitlements misc/dist/macos/editor.entitlements -s - Godot.app
--- scene/2d/physics/character_body_2d.cpp
+++ scene/2d/physics/character_body_2d.cpp
@@ -79,7 +79,12 @@ bool CharacterBody2D::move_h_exact(int32_t p_amount, const Callable &p_callback,
 			{
 				Object *obj = ObjectDB::get_instance(r_result.collider_id);
 				Node2D *collider_body = cast_to<Node2D>(obj);
-				p_callback.call(move_dir_vector, amount_moved, p_amount, collider_body, p_pusher);
+
+				if (p_callback.get_argument_count() == 6) {
+					p_callback.call(move_dir_vector, amount_moved, p_amount, collider_body, r_result.collision_point, p_pusher);
+				} else {
+					WARN_PRINT("move_v collision callback does not have the expected number of arguments (expected 6).");
+				}
 			}
 			return true;
 		}
@@ -109,7 +114,11 @@ bool CharacterBody2D::move_v_exact(int32_t p_amount, const Callable &p_callback,
 			{
 				Object *obj = ObjectDB::get_instance(r_result.collider_id);
 				Node2D *collider_body = cast_to<Node2D>(obj);
-				p_callback.call(move_dir_vector, amount_moved, p_amount, collider_body, p_pusher);
+				if (p_callback.get_argument_count() == 6) {
+					p_callback.call(move_dir_vector, amount_moved, p_amount, collider_body, r_result.collision_point, p_pusher);
+				} else {
+					WARN_PRINT("move_v collision callback does not have the expected number of arguments (expected 6).");
+				}
 			}
 			return true;
 		}
@@ -122,7 +131,11 @@ bool CharacterBody2D::move_v_exact(int32_t p_amount, const Callable &p_callback,
 				{
 					Object *obj = ObjectDB::get_instance(r_results.collider_ids[0]);
 					Node2D *collider_body = cast_to<Node2D>(obj);
-					p_callback.call(move_dir_vector, amount_moved, p_amount, collider_body, p_pusher);
+					if (p_callback.get_argument_count() == 6) {
+						p_callback.call(move_dir_vector, amount_moved, p_amount, collider_body, r_results.collision_points[0], p_pusher);
+					} else {
+						WARN_PRINT("move_v collision callback does not have the expected number of arguments (expected 6).");
+					}
 				}
 				return true;
 			}
